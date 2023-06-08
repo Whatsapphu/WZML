4e74f6f8037c4e64b6da93db35f481dddf608cc3
SourcePlease <134805321+SourcePlease@users.noreply.github.com>
2023-06-08 16:11:04 +0630

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -383,14 +383,14 @@ def get_readable_message():
             buttons.sbutton(f"{PAGE_NO}/{PAGES}", str(THREE))
             buttons.sbutton("Next⏩", "status nex")
             buttons.sbutton("Refresh", "status refresh")
-            buttons.buildbutton(f"Repo", f"https://github.com/weebzone/WZML")
+            #buttons.buildbutton(f"Repo", f"https://github.com/weebzone/WZML")
             buttons.sbutton("Close", "status close")
         else:
             buttons.sbutton("Previous", "status pre")
             buttons.sbutton(f"{PAGE_NO}/{PAGES}", str(THREE))
             buttons.sbutton("Next", "status nex")
             buttons.sbutton("Refresh", "status refresh")
-            buttons.buildbutton(f"Repo", f"https://github.com/weebzone/WZML")
+            #buttons.buildbutton(f"Repo", f"https://github.com/weebzone/WZML")
             buttons.sbutton("Close", "status close")
             button = buttons.build_menu(3)
             return msg + bmsg, button
