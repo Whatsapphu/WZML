de62deee4960e6aeb1438100095a8fae064f34d3
SourcePlease <134805321+SourcePlease@users.noreply.github.com>
2023-06-08 17:39:22 +0630

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -259,7 +259,7 @@ def get_readable_message():
                 msg += f"<code>{escape(str(download.name()))}</code>"
             if download.status() not in [MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_CONVERTING, MirrorStatus.STATUS_QUEUEDL, MirrorStatus.STATUS_QUEUEUP]:
                 if config_dict['EMOJI_THEME']:
-                    msg += f"<b>《🥱 <a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a> 🥱》</b>\n\n"
+                    msg += f"<b>《🥱 <a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a> 🥱》</b>\n  \n"
                     msg += f"\n<b></b>{get_progress_bar_string(download)} {download.progress()}"
                     msg += f"\n<b>🔄 Done:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>⚡ Speed:</b> {download.speed()}"
@@ -268,7 +268,7 @@ def get_readable_message():
                     msg += f"\n<b>⛓️ Engine:</b> {download.eng()}"
                     
                 else:
-                    msg += f"<b>《🥱 <a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a> 🥱》</b>\n\n"
+                    msg += f"<b>《🥱 <a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a> 🥱》</b>\n  \n"
                     msg += f"\n<b></b>{get_progress_bar_string(download)} {download.progress()}"
                     msg += f"\n<b>Done:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>Speed:</b> {download.speed()}"
