e7122d6491596d064bed715ac42d6e2b4467b895
SourcePlease <134805321+SourcePlease@users.noreply.github.com>
2023-06-08 18:08:48 +0630

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -253,16 +253,16 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[int(COUNT):], start=1):
             if config_dict['SAFE_MODE']:
-                msg += f"<b>《🥱<a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a>🥱》</b>\n\n"
+                msg += f"<b><a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a></b>\n\n"
                 msg += f"<b>{download.status()}: Safe Mode Enabled</b>"
             else:
-                msg += f"<b>《🥱<a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a>🥱》</b>\n\n"
+                msg += f"<b><a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a></b>\n\n"
                 msg += f"<b><a href='{download.message.link}'>{download.status()}</a>: </b>"
                 msg += f"<code>{escape(str(download.name()))}</code>"
                # msg += f"<b>《<ahref='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a>》</b>\n\n"
             if download.status() not in [MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_CONVERTING, MirrorStatus.STATUS_QUEUEDL, MirrorStatus.STATUS_QUEUEUP]:
                 if config_dict['EMOJI_THEME']:
-                    msg += f"<b>《🥱<a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a>🥱》</b>\n\n"
+                    msg += f"<b><a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a></b>\n\n"
                     msg += f"\n<b></b>{get_progress_bar_string(download)} {download.progress()}"
                     msg += f"\n<b>🔄 Done:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>⚡ Speed:</b> {download.speed()}"
@@ -271,7 +271,7 @@ def get_readable_message():
                     msg += f"\n<b>⛓️ Engine:</b> {download.eng()}"
                     
                 else:
-                    msg += f"<b>《🥱<a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a>🥱》</b>\n\n"
+                    msg += f"<b><a href='https://t.me/+2hoczTe39cI1Y2Rh'>𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝚂𝚘𝚞𝚛𝚌𝚎𝙿𝚕𝚎𝚊𝚜𝚎</a></b>\n\n"
                     msg += f"\n<b></b>{get_progress_bar_string(download)} {download.progress()}"
                     msg += f"\n<b>Done:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                     msg += f"\n<b>Speed:</b> {download.speed()}"
